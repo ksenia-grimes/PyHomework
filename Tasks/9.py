dcbd5e25177ee8d8a9839680555b6d20da18f614
Ksenia Grimes <71591544+ksenia-grimes@users.noreply.github.com>
2020-10-30 12:59:09 +0700

    Create 9.py

---
+++ Tasks/9.py
@@ -0,0 +1,41 @@
+# задача на включене элементов в список (конкретно после 2 элемента)
+
+# Ввод данных
+spisok = input('Введите список любых целых чисел: ')
+guide = ['1','2','3','4','5','6','7','8','9','0',',',' ']
+for i in spisok:
+	if i not in guide:
+		print ('Некорректный ввод. Вводите только целые числа.')
+		exit()
+#есть проблема ввода без запятых (3 8 4 8 43), он тогда не может создать список и программа не работает
+
+probel = spisok.replace(" ", "")
+numbers = probel.split(',')
+
+#проверка трех элементов
+a1 = input ('Введите искомое число: ')
+for p in a1:
+	if p not in guide:
+		print ('Некорректный ввод искомого числа.')
+		exit()
+a11=a1.replace(' ','')
+
+a2 = input ('Введите искомое число: ')
+for p in a2:
+	if p not in guide:
+		print ('Некорректный ввод искомого числа.')
+		exit()
+a22 = a2.replace(' ','')
+
+a3 = input ('Введите искомое число: ')
+for p in a3:
+	if p not in guide:
+		print ('Некорректный ввод искомого числа.')
+		exit()
+a33 = a3.replace (' ','')
+
+numbers.insert(2, a11)
+numbers.insert(3, a22)
+numbers.insert(4, a33)
+
+print(numbers)
