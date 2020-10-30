ac93dd4784ee0e0de0da1af2da26023883e3918a
Ksenia Grimes <71591544+ksenia-grimes@users.noreply.github.com>
2020-10-30 13:54:17 +0700

    Create 12.py

---
+++ Tasks/12.py
@@ -0,0 +1,32 @@
+# Дан список чисел. Вычислить среднее арифметическое положительных элементов этого списка
+# и среднее арифметическое отрицательных элементов этого списка (0 исключать, он ни положителен, ни отрицателен)
+
+# Ввод
+spisok = input("Пожалуйста, введите любую последовательность чисел: ")
+
+probel = spisok.replace(" ", "")
+numbers = probel.split(',')
+
+try:
+	nums = [int(item) for item in numbers]
+except ValueError:
+	print ('Некорректный ввод, вводите только целые числа.')
+	exit()
+
+# Решение
+plus = []
+minus = []
+#среднее отрицательных элементов
+
+for j in nums:
+    if j < 0:
+        minus.append(j)
+resultMIN = sum(minus) / len(minus)
+print(resultMIN)
+
+#среднее положительных элементов
+for i in nums:
+    if i > 0:
+        plus.append(i)
+resultPLUS = sum(plus) / len(plus)
+print(resultPLUS)
