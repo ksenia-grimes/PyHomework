f53ce5adc7c46107127d0ba92ac2b577c30fec0f
Ksenia Grimes <71591544+ksenia-grimes@users.noreply.github.com>
2020-12-04 12:13:48 +0700

    Update defTask_1.py

--- Lesson6/defTask_1.py
+++ Lesson6/defTask_1.py
@@ -1,17 +1,16 @@
-task_results = []
-final_results = []
 def Task(functions,input_list):
+    task_results = []
+    final_results = []
     for x in input_list:
         task_results.append(functions[0](x))
     for y in task_results:
         final_results.append(functions[1](y))
-    return final_results
+    return max(final_results)
 def F1(x):
-    return x*2
+    return x*2 #содержит в себе результат F1
 def F2(x):
     return x*3
 
 functions = [F1,F2]
 input_list = [1,2,3]
 print (Task(functions, input_list))
-print (max(final_results))
