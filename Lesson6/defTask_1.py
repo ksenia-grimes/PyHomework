b903fc4e1d481f463899b9ded1b5f4b94a3f51a1
Ksenia Grimes <71591544+ksenia-grimes@users.noreply.github.com>
2020-12-04 12:59:19 +0700

    Update defTask_1.py

--- Lesson6/defTask_1.py
+++ Lesson6/defTask_1.py
@@ -2,9 +2,9 @@ def Task(functions,input_list):
     task_results = []
     final_results = []
     for x in input_list:
-        task_results.append(functions[0](x))
+        task_results.append(functions[0](x)) #выполняет F1 для каждого элемента входного списка и помещает результат в новый список task.results
     for y in task_results:
-        final_results.append(functions[1](y))
+        final_results.append(functions[1](y)) #выполняет F2 для каждого элемента нового списка task.results (список с результатами F1) и помещает результат в final_results
     return max(final_results)
 def F1(x):
     return x*2
