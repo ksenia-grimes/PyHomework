75c12034d70a1f62b65b43deba0e60316cc418cd
Ksenia Grimes <71591544+ksenia-grimes@users.noreply.github.com>
2020-12-04 14:59:11 +0700

    Create defTask_2.py

---
+++ Lesson6/defTask_2.py
@@ -0,0 +1,14 @@
+def Task(functions,input_list):
+    copylist = input_list.copy()
+    for f in functions:
+        for x in range(len(copylist)):
+            copylist[x] = f(copylist[x])
+    return max(copylist) 
+    
+def F1(x):
+    return x*2 
+def F2(x):
+    return x*3
+functions = [F1,F2]
+input_list = [1,2,3]
+print (Task(functions, input_list))
