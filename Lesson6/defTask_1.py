9e2f36d9301110d311bacef7cc6699d66c23b35f
Ksenia Grimes <71591544+ksenia-grimes@users.noreply.github.com>
2020-12-04 12:07:08 +0700

    Create defTask_1.py

---
+++ Lesson6/defTask_1.py
@@ -0,0 +1,17 @@
+task_results = []
+final_results = []
+def Task(functions,input_list):
+    for x in input_list:
+        task_results.append(functions[0](x))
+    for y in task_results:
+        final_results.append(functions[1](y))
+    return final_results
+def F1(x):
+    return x*2 #содержит в себе результат F1
+def F2(x):
+    return x*3
+
+functions = [F1,F2]
+input_list = [1,2,3]
+print (Task(functions, input_list))
+print (max(final_results))
